2e7934a21074ba9eb9c21099aff80f5c31a31613
Elizabeth Rivera <elizabethriver@gmail.com>
2020-12-02 12:41:14 -0500

    realizar test

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,9 +1,15 @@
-const {pathisAbsolute, validationPath, pathBecameAbsolute} = require('../lib/util.js')
+const {validationPath, pathisAbsolute, pathBecameAbsolute, files, stats, extname, isDirectory, isFile, fileMd, pathAbsolute, readFile} = require('../lib/util.js')
 
 const absPath = 'C:\\Users\\ELIZABETH\\Documents\\Laboratoria\\LIM013-fe-md-links\\test\\example\\wiki.md';
 const relaPath = './test/example/texto.tx';
 const convertAbsPath = 'C:\\Users\\ELIZABETH\\Documents\\Laboratoria\\LIM013-fe-md-links\\test\\example\\texto.tx';
 const fakePath = '../test/example/texto.js';
+const filesReaded = ["example", "md-links.spec.js", "util.spec.js"];
+const directpath = 'C:\\Users\\ELIZABETH\\Documents\\Laboratoria\\LIM013-fe-md-links\\test\\';
+const arrayFileMD = ['test\\example\\wiki.md'];
+const fileMdTest = './test/example/wiki.md';
+const arrayEmpty = [];
+const textFile = '[wiki](https://es.wikipedia.org/wiki/Wikipedia:Portada)';
 
 //TODO revisar 
 describe('pathBecameAbsolute', () => {
@@ -39,3 +45,69 @@ describe('pathisAbsolute', () => {
     });
   });
 
+  describe('files', () => {
+    it('files es una función', () => {
+      expect(typeof files).toBe('function');
+    });
+    it('Devuelve files', () => {
+      expect(files(directpath)).toEqual(filesReaded);
+    });
+  });
+
+  describe('extname', () => {
+    it('extname es una función', () => {
+      expect(typeof extname).toBe('function');
+    });
+    it('Devuelve la ultima extension del file', () => {
+      expect(extname(fakePath)).toEqual('.js');
+    });
+  });
+
+  describe('isDirectory', () => {
+    it('isDirectory es una función', () => {
+      expect(typeof isDirectory).toBe('function');
+    });
+    it('Devuelve true si es una ruta valida', () => {
+      expect(isDirectory(directpath)).toEqual(true);
+    });
+    it('Devuelve false si no es una ruta valida', () => {
+      expect(isDirectory(absPath)).toEqual(false);
+    });
+  });
+
+  describe('isFile', () => {
+    it('isFile es una función', () => {
+      expect(typeof isFile).toBe('function');
+    });
+    it('Devuelve true si es una ruta valida', () => {
+      expect(isFile(absPath)).toEqual(true);
+    });
+    it('Devuelve false si no es una ruta valida', () => {
+      expect(isFile(directpath)).toEqual(false);
+    });
+  });
+
+  describe('fileMd', () => {
+    it('fileMd es una función', () => {
+      expect(typeof fileMd).toBe('function');
+    });
+    it('Devuelve un array de .md file', () => {
+      expect(fileMd(fileMdTest)).toEqual(arrayFileMD);
+    });
+    it('Devuelve un array vacio porque no hay .md file', () => {
+      expect(fileMd(relaPath)).toEqual(arrayEmpty);
+    });
+  });
+  describe('stats', () => {
+    it('stats es una función', () => {
+      expect(typeof stats).toBe('function');
+    });
+  });
+  describe('readFile', () => {
+    it('stats es una función', () => {
+      expect(typeof readFile).toBe('function');
+    });
+    it('Devuelve texto de un file', () => {
+      expect(readFile(fileMdTest)).toEqual(textFile);
+    });
+  });
